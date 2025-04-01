90a81e7075c652b94dcbd5b6dd32df088cecbe2f
Julian Stamm <mail@julcs.com>
2025-04-01 22:21:36 +0200

    Add hit strength

--- player.gd
+++ player.gd
@@ -3,16 +3,27 @@ extends RigidBody3D
 class_name Player
 
 @export var strength := 1.0
+@export var strength_curve: Curve
 
 var checkpoint: Vector3
+var press_time: float = NAN
 
 func _ready() -> void:
 	checkpoint = global_position
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("hit"):
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("hit"):
+		press_time = Time.get_unix_time_from_system()
+	elif event.is_action_released("hit"):
+		var press_length := Time.get_unix_time_from_system() - press_time
+		press_time = NAN
+		var press_strength := strength_curve.sample(press_length)
 		var dir := Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
-		apply_central_impulse(Vector3(dir.x, 0, dir.y) * strength)
+		hit(dir, press_strength * strength)
+		Input.start_joy_vibration(event.device, 0, press_strength, 0.1)
+
+func hit(dir: Vector2, strength: float):
+	apply_central_impulse(Vector3(dir.x, 0, dir.y) * strength)
 
 func reset() -> void:
 	global_position = checkpoint
