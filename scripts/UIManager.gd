a9b649670c5d6cad61daa66442860f28e5236117
Josh Jaekel <s64jjaek@uni-bonn.de>
2025-04-02 19:45:45 +0200

    Szene geändert

--- scripts/UIManager.gd
+++ scripts/UIManager.gd
@@ -6,7 +6,8 @@ func _ready():
 	for i in range(4): 
 		print(positions)
 		var box = get_child(0).get_child(i)
-		positions[i] = box.get_screen_transform().origin
+		print(box)
+		positions[i] = box.get_global_transform().origin
 	Global.update_UI_pos(positions)
-	print(Global.player_UI_Box_Pos)
+	
 	print(positions)
