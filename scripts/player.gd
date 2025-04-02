bb7507e4c0cdf14c893d8ec7ced7d540c345b240
Julian Stamm <mail@julcs.com>
2025-04-02 15:39:34 +0200

    Multiplayer

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,43 +5,49 @@ class_name Player
 @export var strength := 1.0
 @export var strength_curve: Curve
 @export var device := 0
-@export var arrow: Node3D
-@onready var shaft: Node3D = arrow.get_child(0)
-@onready var head: Node3D = arrow.get_child(1)
+@export var arrow_length := 20.0
+
+@onready var arrow := $Arrow
+@onready var shaft := $Arrow/Shaft
+@onready var head := $Arrow/Head
 
 var checkpoint: Vector3
-var press_time: float = NAN
-
-func _ready() -> void:
-	checkpoint = global_position
+var press_time: float = 0.0
+var aim: Vector2
+var power := 0.0
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("hit"):
+	if not event.device == device:
+		return
+	elif event.is_action_pressed("hit"):
 		press_time = Time.get_unix_time_from_system()
-		device = event.device
 	elif event.is_action_released("hit"):
 		var press_length := Time.get_unix_time_from_system() - press_time
-		var press_strength := strength_curve.sample(press_length)
-		var dir := Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
-		hit(dir, press_strength * strength)
-		Input.start_joy_vibration(event.device, 0, press_strength, 0.1)
-		press_time = NAN
+		power = strength_curve.sample(press_length)
+		hit(aim, power * strength)
+		Input.start_joy_vibration(device, power, power, 0.05)
+		press_time = 0.0
+	elif event is InputEventJoypadMotion:
+		if event.axis == JOY_AXIS_LEFT_X:
+			aim.x = event.axis_value
+		elif event.axis == JOY_AXIS_LEFT_Y:
+			aim.y = event.axis_value
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	for contact_idx in state.get_contact_count():
 		var object_hit := state.get_contact_collider_object(contact_idx)
 		if is_instance_valid(object_hit): # To fix a case where an object hits the player as player is deleted during level transition (intermission)
 			var impact := state.get_contact_impulse(contact_idx).length()
-			if impact > 1.4:
+			if impact > 0.5:
 				print(impact)
-				Input.start_joy_vibration(device, 0, impact * 100.0, 0.1)
+				Input.start_joy_vibration(device, impact, impact, 0.05)
 
 func _process(delta: float) -> void:
-	var dir := Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
-	var press_length := Time.get_unix_time_from_system() - press_time
-	var press_strength := strength_curve.sample(press_length)
-	var dir3d := Vector3(dir.x, 0, dir.y)
-	update_arrow(dir3d, press_strength * 20.0)
+	if press_time:
+		var press_length := Time.get_unix_time_from_system() - press_time
+		power = strength_curve.sample(press_length)
+	var dir3d := Vector3(aim.x, 0, aim.y)
+	update_arrow(dir3d * power * arrow_length)
 
 func hit(dir: Vector2, strength: float):
 	apply_central_impulse(Vector3(dir.x, 0, dir.y) * strength)
@@ -52,18 +58,12 @@ func reset() -> void:
 	set_deferred("angular_velocity", 0)
 	set_deferred("constant_force", Vector2.ZERO)
 
-func update_arrow(dir: Vector3, length: float):
-	if length and dir and is_finite(length):
+func update_arrow(dir: Vector3):
+	if dir.length_squared() > 1e-3:
 		arrow.show()
 		arrow.look_at_from_position(global_position, global_position - dir)
+		var length := dir.length()
 		shaft.scale.z = length
 		head.position.z = length
 	else:
 		arrow.hide()
-	#shaft.basis = Basis()
-	#shaft.transform = shaft.transform.scaled(Vector3(1,dir2.length(),1))
-	#transform = Transform3D(basis, target.transform.origin)
-	#var angle = atan2(dir.x,dir.z) + deg_to_rad(180)
-	#body.basis = Basis(Vector3(0,0,1),angle)
-	#body.basis = body.basis.rotated(Vector3(1,0,0),deg_to_rad(-90))
-	#body.transform = body.transform.scaled(Vector3(0.05,0.05,0.05))
